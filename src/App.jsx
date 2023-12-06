7131645fa9f81825255dd22873c9c33d056c8d8a
grsmith44 <grsmith44@tntech.edu>
2023-12-05 23:14:46 -0600

    Added CSS file and gave the project much better styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import reducer from "./component/reducer.jsx";
 import { ADD_NUM, OPERATOR, CLEAR, CALC } from "./component/actions.jsx";
+import "./App.css";
 
 const defaultState = {
   dig: " ",
@@ -37,46 +38,43 @@ function App() {
   const [state, dispatch] = useReducer(reducer, defaultState);
   const { dig, prevDig, operation } = state;
   return (
-    <>
-      <div style={{ alignItems: "right" }}>
-        <p>
-          {prevDig}
-          {operation}
-        </p>
-        <p id="display">{dig}</p>
+    <div className="calculator-case">
+      <div className="ds-outer-background">
+        <div className="display-screen">
+          <p>{operation}</p>
+          <p id="display">{dig}</p>
+        </div>
+        <p className="signature">Programmed by Gage Smith. 2023</p>
       </div>
-      <div>
+
+      <div className="button-pad">
         <button id="clear" onClick={() => dispatch({ type: CLEAR })}>
           AC
         </button>
         <button id="equals" onClick={() => dispatch({ type: CALC })}>
           =
         </button>
-      </div>
-      <div>
+
         <NumButton digit="7" dispatch={dispatch} />
         <NumButton digit="8" dispatch={dispatch} />
         <NumButton digit="9" dispatch={dispatch} />
         <OpButton operation="-" dispatch={dispatch} id="minus" />
-      </div>
-      <div>
+
         <NumButton digit="4" dispatch={dispatch} />
         <NumButton digit="5" dispatch={dispatch} />
         <NumButton digit="6" dispatch={dispatch} />
         <OpButton operation="+" dispatch={dispatch} id="add" />
-      </div>
-      <div>
+
         <NumButton digit="1" dispatch={dispatch} />
         <NumButton digit="2" dispatch={dispatch} />
         <NumButton digit="3" dispatch={dispatch} />
         <OpButton operation="*" dispatch={dispatch} id="multiply" />
-      </div>
-      <div>
+
         <NumButton digit="0" dispatch={dispatch} />
         <NumButton digit="." dispatch={dispatch} />
         <OpButton operation="/" dispatch={dispatch} id="divide" />
       </div>
-    </>
+    </div>
   );
 }
 
